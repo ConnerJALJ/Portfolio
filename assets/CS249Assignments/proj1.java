4d2cd3f19257a5097c6143ed14560ff35a5de9ac
ConnerJALJ <connerjalj@gmail.com>
2018-09-18 02:59:23 -0400

    created

---
+++ assets/CS249Assignments/proj1.java
@@ -0,0 +1,11 @@
+/* Hello world program
+ * CS249 with Spetka
+ * September 2018
+ * Language: Java (javac target)
+ */
+
+public class proj1 {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}

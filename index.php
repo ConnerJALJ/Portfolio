1605cdf5fbd0fa98bb7a12dff8639739af31af2b
ConnerJALJ <connerjalj@gmail.com>
2018-09-27 03:53:32 -0400

    got rid of inclusions and tyring to fix href to cs249

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
   <head>
     <title>Conner LaPage-Jenkins' Portfolio</title>
     <!-- Required meta tags -->
-    <?php require 'components/inclusions.php';?>
-    <!--
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
-    -->
   </head>
   <!--Style the body's background for all browsers-->
   <body style="background: url(assets/images/triangles-1430105.svg) no-repeat center center fixed; 
@@ -27,7 +24,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="navbar-nav">
           <a href="/" class="nav-item nav-link active">Home<span class="sr-only">(current)</span></a>
-          <a href="CS249" class="nav-item nav-link">CS249 Assignments</a>
+          <a href="CS249.php" class="nav-item nav-link">CS249 Assignments</a>
           <a href="#" class="nav-item nav-link">Coming Soon</a>
         </div>
       </div>  
@@ -84,10 +81,8 @@
     <!--End Main Cards-->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!--
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
--->
   </body>
 </html>

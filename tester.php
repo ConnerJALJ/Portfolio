c1dd5a98c4fc6c2ec84ad6de8016e09b1d9561ca
ConnerJALJ <connerjalj@gmail.com>
2018-10-02 02:35:24 -0400

    i need to see if it is even loading my changes

--- tester.php
+++ tester.php
@@ -1,12 +1,26 @@
 <!doctype html>
 <html>
-<body>
+    <head>
+        <title>CS249 Assignments</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<?php
-echo "My first PHP script!";
-?>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <!--Style the body's background for all browsers-->
+    <body style="background: url(assets/images/triangles-1430105.svg) no-repeat center center fixed; 
+                 -webkit-background-size: cover;
+                 -moz-backgournd-size: cover;
+                 -o-backgournd-size: cover;
+                 background-size: cover;">
 
-<h1>AUHOOAHFHOAIDHO</h1>
+        <?php
+        echo "My first PHP script!";
+        ?>
 
+        <h1>AUHOOAHFHOAIDHO</h1>
 </body>
 </html>

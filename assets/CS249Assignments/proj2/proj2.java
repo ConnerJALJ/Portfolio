ecb14c74e76eff6cb112d433a15e03cb4238cd0b
Conner Jenkins <you@example.com>
2019-09-05 15:50:42 -0400

    created week1-hw and week2-hw folder moved galleyMenu.html and parkBookstore.html too new week1-hw folder finished hw3 pgmExplanation.html added hw3 to CS351.html added picture of hw3 to assets removed .html tags from galleyMenu and parkBookstore href

--- assets/CS249Assignments/proj2/proj2.java
+++ assets/CS249Assignments/proj2/proj2.java
@@ -1,10 +1,12 @@
+
 /****************************************ASSIGNMENT DESCRIPTION*********************************************************
  * Write a program that calculates the total grade for three classroom exercises as a
  * percentage. Use the DecimalFormat class to output the value as a percent. The
  * scores should be summarized in a table. Input the assignment information in this
  * order: name of assignment (may include spaces), points earned (integer), and total
  * points possible (integer). The percentage is the sum of the total points earned divided
- * by the total points possible. Sample input and output is shown as follows:
+ * by the total points possible. 
+ * Sample input and output is shown as follows:
  *
  * Exercise 		Score   Total Possible
  * Group Project     10 		10
@@ -19,23 +21,20 @@
  * Language: Java (javac target)
 ***********************************************************************************************************************/
 
-
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
 
-public class proj2
-{
-    public static void main(String[] args)
-    {
+public class proj2 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] nameOfExcercises = new String[3];
         int[] pointsEarned = new int[3];
         int[] pointsPossible = new int[3];
         int totalPointsEarned = 0, totalPointsPossible = 0;
 
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             System.out.println("Enter the name of the exercise: ");
             nameOfExcercises[i] = input.nextLine();
 
@@ -49,7 +48,7 @@ public class proj2
 
             input.nextLine();
         }
-        
+
         DecimalFormat useformat = new DecimalFormat("#.00%");
         input.close();
 
@@ -60,7 +59,7 @@ public class proj2
         System.out.printf("%-20s %-10d %-10d%n", "Total", totalPointsEarned, totalPointsPossible);
 
         System.out.print("\nYour total is " + totalPointsEarned + " out of " + totalPointsPossible);
-        System.out.println(", or " + useformat.format((double)totalPointsEarned / totalPointsPossible) + ".");
+        System.out.println(", or " + useformat.format((double) totalPointsEarned / totalPointsPossible) + ".");
 
     }
 }
